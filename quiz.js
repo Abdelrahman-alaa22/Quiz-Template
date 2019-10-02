e9f39df4d027a16f5c3a10f37339a0bfcd0e53a7
Abdelrahman-alaa22 <abdelrahman.alaa4497@gmail.com>
2019-10-02 03:42:20 +0200

    Animation added

--- quiz.js
+++ quiz.js
@@ -8,6 +8,7 @@ const result = document.querySelector('.result');
 form.addEventListener('submit', e => {
     e.preventDefault();
 
+    //Matching Answers
     let score = 0;
     let userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
@@ -17,9 +18,23 @@ form.addEventListener('submit', e => {
         }
     });
 
-    scrollTo(0,0);
+    // To go to the top of the page after submiting
+    scrollTo(0,0); 
+
     result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
+
+    // Adding number animation
+    let ouput = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${ouput}%`;
+        if(ouput === score){
+            clearInterval(timer);
+        } else {
+            ouput++;
+        }
+    }, 10);
+
 
 });
 
